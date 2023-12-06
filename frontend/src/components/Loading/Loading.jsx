d574e4aefb70945195ea11095601bb0abacb2e08
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-12-06 04:06:14 -0300

    add: Component Loading

---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Loading from './Spin-1s-200px.gif'
+
+const LoadingComponent = () => {
+  return (
+    <img src={Loading} className='w-[300px] h-[300px]'/>
+  )
+}
+
+export default LoadingComponent
