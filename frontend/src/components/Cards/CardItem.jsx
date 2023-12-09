373548b7f9dee10c3c192c5655c4a8a71e635ec8
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-12-09 08:13:04 -0300

    feat: resp CardItem mobile

--- frontend/src/components/Cards/CardItem.jsx
+++ frontend/src/components/Cards/CardItem.jsx
@@ -7,8 +7,56 @@ import { Button, CardActionArea, CardActions } from '@mui/material';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function MultiActionAreaCard({id,image,produto,disponivel,category,price}) {
+
+
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowSize = window.innerWidth;
+      const isMobile = windowSize <= 640;
+      setIsMobile(isMobile);
+    };
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("orientationchange", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("orientationchange", handleResize);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <Link to={`/product/${id}`} className={`border-2 mx-8 max-[400px]:mx-1 ${disponivel ? 'border-green-600' : 'border-red-300'} w-full h-[150px] flex flex-wrap rounded-xl`}>
+        <div className='w-[35%] h-[150px] max-[400px]:w-[45%]'>
+          <img src={image} alt="" className='w-[150px] h-[150px] py-1 rounded-xl border-r-[1px]'/>
+        </div>
+        <div className='flex flex-col w-[65%] max-[400px]:w-[55%] items-center'>
+          <div>
+            <p className='text-lg font-bold text-gray-700 text-center'>{produto}</p>
+          </div>
+          <div>
+            <p className='text-md text-gray-500 font-semibold'>{category}</p>
+          </div>
+          <div>
+            <p className='text-sm text-gray-900 font-semibold'>R$ {price}</p>
+          </div>
+          <div>
+            <Button className={`${disponivel ? null : 'bg-red-300'}`} size="small" color="primary">
+              {disponivel ? <p className='text-white bg-green-500 py-2 px-2 rounded-md'>Disponivel</p> : <p className='text-white bg-red-300  py-2 px-2 rounded-md'>Indisponivel</p>}
+            </Button>
+          </div>
+        </div>
+      </Link>
+    )
+  }
 
   return (
     <Link to={`/product/${id}`}>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -58,35 +58,35 @@ const Product = () => {
   return (
     <div className='mb-32'>
         <PrimaryTitle botaoVoltar={true} title='Descrição do item:'/>
-        <div className='flex justify-evenly'>
-            <div className='mt-12 flex flex-col gap-3'>
+        <div className='flex justify-evenly max-[800px]:flex-col'>
+            <div className='mt-12 flex flex-col gap-3 max-[800px]:mx-12 max-[530px]:mx-4'>
                 <div>
-                    <h1 className='text-3xl'>Nome do Produto: </h1>
-                    {isEditing ? <input type='text' onChange={(e) => setName(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.name}/> : <p className='text-gray-700 font-semibold'>{dados.name}</p>}
+                    <h1 className='text-3xl max-[510px]:text-lg font-bold'>Nome do Produto: </h1>
+                    {isEditing ? <input type='text' onChange={(e) => setName(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.name}/> : <p className='text-gray-700 font-semibold text-sm'>{dados.name}</p>}
                 </div>
                 <div>
-                    <h1 className='text-3xl'>Preço do Produto:</h1>
-                    {isEditing ? <input type='text' onChange={(e) => setPrice(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.price}/> : <p className='text-gray-700 font-semibold'>{dados.price}</p>}
+                    <h1 className='text-3xl max-[510px]:text-xl font-bold'>Preço do Produto:</h1>
+                    {isEditing ? <input type='text' onChange={(e) => setPrice(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.price}/> : <p className='text-gray-700 font-semibold text-sm'>{dados.price}</p>}
                 </div>
                 <div>
-                    <h1 className='text-3xl'>Categoria do produto:</h1>
-                    {isEditing ? <input type='text' onChange={(e) => setCategory(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.category}/> : <p className='text-gray-700 font-semibold'>{dados.category}</p>}
+                    <h1 className='text-3xl max-[510px]:text-xl font-bold'>Categoria do produto:</h1>
+                    {isEditing ? <input type='text' onChange={(e) => setCategory(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.category}/> : <p className='text-gray-700 font-semibold text-sm'>{dados.category}</p>}
                 </div>
                 <div>
-                    <h1 className='text-3xl'>Link da imagem do produto:</h1>
-                    {isEditing ? <input type='text' onChange={(e) => setImage(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.image}/> : <p className='text-gray-700 font-semibold'>{dados.image}</p>}
+                    <h1 className='text-3xl max-[510px]:text-xl font-bold'>Link da imagem do produto:</h1>
+                    {isEditing ? <input type='text' onChange={(e) => setImage(e.target.value)} className='border-2 border-gray-300 rounded-md w-[300px] h-[40px] px-2' placeholder={dados.image}/> : <p className='text-gray-700 font-semibold text-sm'>{dados.image}</p>}
                 </div>
                 <div>
-                    <h1 className='text-3xl'>Disponibilidade do produto:</h1>
+                    <h1 className='text-3xl max-[510px]:text-xl font-bold'>Disponibilidade do produto:</h1>
                     {isEditing ? (
                         <div className='text-white font-bold flex justify-center gap-5'>
                             <button onClick={() => setDisponivel(true)} className={` ${disponivel ? 'bg-green-700' : 'bg-gray-500'} px-3 py-2 rounded-lg duration-300`}>Disponivel</button> 
                             <button className={` ${disponivel ? 'bg-gray-500' : 'bg-red-700'} px-3 py-2 rounded-lg duration-300`} onClick={() => setDisponivel(false)}>Indisponivel</button>
                         </div>
-                    ): <p className='text-gray-700 font-semibold'>{dados.disponivel ? 'Disponivel' : 'Indisponivel'}</p>}
+                    ): <p className='text-gray-700 font-semibold text-sm'>{dados.disponivel ? 'Disponivel' : 'Indisponivel'}</p>}
                 </div>
             </div>
-            <div>
+            <div className='max-[800px]:flex justify-center'>
                 {isEditing ? <img src={dados.image} className='w-[300px] h-[300px] rounded-md'/> : <img src={dados.image} className='w-[300px] h-[300px] rounded-md'/>}
             </div>
         </div>
